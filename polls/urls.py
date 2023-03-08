5fab1527514264fd98e77e6638fe841e57db3f70
Cesar Roman <cesardroman18@gmail.com>
2023-03-08 11:35:00 -0600

    edited typo

--- polls/urls.py
+++ polls/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     #/polls/5
     path('<int:question_id>', views.detail, name='detail'),
     #/polls/5/results
-    path('<int:question_id>/results', views.results, name='results')
+    path('<int:question_id>/results', views.results, name='results'),
     #/polls/5/vote
     path('<int:question_id>/vote', views.vote, name='vote'),
 ]
