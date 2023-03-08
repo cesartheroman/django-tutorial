9919458d659bde1a3067891b91323bd43fa0bc82
Cesar Roman <cesardroman18@gmail.com>
2023-03-08 12:58:44 -0600

    added templating + error handling in views

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
 from django.template import loader
+
 
 from .models import Question
 
@@ -22,7 +23,15 @@ def index(request):
     # return HttpResponse(output)
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+
+    return render(request, 'polls/detail.html',{'question': question})
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404('Question does not exist')
+    
+    # return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
